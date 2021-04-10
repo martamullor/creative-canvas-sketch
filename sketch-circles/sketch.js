0f14bfdc75523c16badc775ad05bfc54fb5d6a73
Marta Mullor <marta.mullor.polo@gmail.com>
2021-04-10 11:39:11 +0200

    Create canvas with text

--- sketch-circles/sketch.js
+++ sketch-circles/sketch.js
@@ -1,7 +1,10 @@
 const canvasSketch = require('canvas-sketch');
+/*Linear interpolation function */
 const { lerp } = require('canvas-sketch-util/math');
 const { range, rangeFloor, noise2D } = require('canvas-sketch-util/random');
+/*Random number generation */
 const random = require('canvas-sketch-util/random');
+/*Random color palettes that appear in array */
 const palettes = require('nice-color-palettes');
 
 
@@ -12,12 +15,16 @@ const settings = {
 const sketch = () => {
 
   const colorCount = rangeFloor(1, 6)
+  /* Return only one array */
+  /* With slice we can use only a limited number of colors that we want */
   const palette = random.shuffle(random.pick(palettes).slice(0, colorCount))
-  const symbols = ['=', '.'];
 
+  /* Return the point on the grid */
   const createGrid = () => {
     const points = [];
-    const count = 50;
+    /* Count is the grid size */
+    const count = 30;
+    /* Distribute around the grid count working in two dimensions */
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         /* Something called UV space */
@@ -26,15 +33,12 @@ const sketch = () => {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
         /* Push the points of the grid */
-        const radius = Math.abs(random.noise2D(u, v) * 0.1)
-        //const radius = Math.abs(random.noise2D(0.06, 1) * 0.2);
+        const radius = Math.abs(random.noise2D(u, v) * 0.03)
         points.push({
           /* Random value times random.value() * 0.01 */
           /* Use gaussian with not a grid effect we cannot have a negative 
           radius and for that we use Math.abs() */
           radius,
-          symbol: random.pick(symbols),
-          rotation: random.noise2D(u, v) * 2,
           position: [u, v],
           color: random.pick(palette),
         })
@@ -57,20 +61,18 @@ const sketch = () => {
 
     points.forEach(data => {
       /* Destructure de data that comes from the object*/
-      const { position, radius, color, rotation, symbol } = data;
+      const { position, radius, color } = data;
       const [u, v] = position;
       /* To distribute de points around the grid*/
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      context.save();
+      /* Create a form */
+      /* Radius relative from the page radius * width */
+      context.beginPath();
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
       context.fillStyle = color;
-      context.font = `${radius * width}px "Arial"`;
-      context.translate(x, y);
-      context.rotate(rotation);
-      context.fillText(`${symbol}`, 0, 0);
-
-      context.restore();
+      context.fill();
 
     })
   };
